f7112aa59313f3d870a2d75cf1cd0c0cc47981d1
mintteal <minttu.laesmaa@student.hamk.fi>
2023-04-24 19:37:59 +0300

    Schemas for games, questions and categories

---
+++ backend/models/gameModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/* 
+With timestamps set to true, Mongoose will add createdAt and updatedAt properties to the Schema.
+createdAt represents a date when the document was created / inserted
+updatedAt represents a date when the document was last updated (save, update, replace etc.)
+*/
+
+const gameSchema = new Schema(
+  {
+    player: { type: String, required: true },
+    score: { type: Number, required: true },
+    difficulty: {
+      type: String,
+      enum: ['easy', 'medium', 'hard'],
+      default: 'easy',
+    },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
+    questionsAnswered: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+gameSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const GameModel = mongoose.model('Game', gameSchema);
+module.exports = GameModel;
