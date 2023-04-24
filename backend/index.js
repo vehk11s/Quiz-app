bc2821ca81eae877661e241bd058db4f12c5512b
mintteal <minttu.laesmaa@student.hamk.fi>
2023-04-24 19:34:27 +0300

    Created connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
