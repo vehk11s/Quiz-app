7fff2eb50d6ec010b0ec33e97ac981c080c7ae95
mintteal <minttu.laesmaa@student.hamk.fi>
2023-04-24 14:17:37 +0300

    Node.js server
    Set up Node + Express server connection

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
