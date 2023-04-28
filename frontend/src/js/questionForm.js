a723c8617341151b8c8a2d9569e711422e57c3df
mintteal <minttu.laesmaa@student.hamk.fi>
2023-04-28 15:01:21 +0300

    Functionality for removing guestion sets and tracking the visible indexing

--- frontend/src/js/questionForm.js
+++ frontend/src/js/questionForm.js
@@ -1,22 +1,42 @@
 import { postQuestion } from '../api/questionApi.js';
 
-let questions = 1;
+let totalQuestions = 1; // Doesn't decrease when removing a question
+let visibleQuestions = 1; // Tracks the visible question sets on display
 
 let addBtn = document.querySelector('#add-question');
 let submitBtn = document.querySelector('#submit-form');
 
-// Create first question form element
+// Create first question form element on load
 document.addEventListener('DOMContentLoaded', () => {
   addQuestion();
+  updateIndexing();
 });
 
 addBtn.addEventListener('click', (e) => {
-  questions += 1;
+  totalQuestions += 1;
+  visibleQuestions += 1;
   addQuestion();
+  updateIndexing();
   e.preventDefault();
 });
 
 submitBtn.addEventListener('click', (e) => {
+  handleSubmit(e);
+});
+
+/*
+This function handles removing question sets from the form and decreasing the 
+TODO: This doesn't currently update question numbering, just removes the selected question */
+function handleRemove(e) {
+  const form = document.querySelector('.form__content');
+  const parentSet = e.target.closest('fieldset');
+
+  form.removeChild(parentSet);
+  updateIndexing();
+  visibleQuestions -= 1;
+}
+
+function handleSubmit() {
   e.preventDefault();
 
   const questions = document.querySelectorAll('.form__question');
@@ -39,18 +59,29 @@ submitBtn.addEventListener('click', (e) => {
     formData.push({ question: formValues[0].value, options });
   });
 
-  console.log(formData);
-
   postQuestion(formData);
-});
+}
 
-const addQuestion = () => {
+function updateIndexing() {
+  const legends = document.querySelectorAll('.form__question > legend');
+  console.log(legends);
+
+  legends.forEach((legend, i) => {
+    legend.textContent = '';
+    legend.textContent = `${i + 1}. Question`;
+  });
+}
+
+function addQuestion() {
   const form = document.querySelector('.form__content');
 
-  const questionSet = createFieldset('question', `${questions}. Question`);
+  const questionSet = createFieldset(
+    'question',
+    `${visibleQuestions}. Question`
+  );
 
-  const questionLabel = createLabel(`Q${questions}`, 'Question');
-  const questionInput = createInput(`Q${questions}`);
+  const questionLabel = createLabel(`Q${totalQuestions}`, 'Question');
+  const questionInput = createInput(`Q${totalQuestions}`);
 
   questionLabel.appendChild(questionInput);
 
@@ -58,7 +89,7 @@ const addQuestion = () => {
   questionSet.appendChild(createOptionInputs());
 
   form.appendChild(questionSet);
-};
+}
 
 function createOptionInputs() {
   // Create fieldset & legend for the options
@@ -100,12 +131,33 @@ function createFieldset(group, legendText) {
   const fieldset = document.createElement('fieldset');
   fieldset.classList.add(`form__${group}`);
 
+  if (group === 'question') {
+    fieldset.setAttribute('id', `Q${totalQuestions}-group`);
+    fieldset.appendChild(createRemoveBtn());
+  }
+
   const legend = document.createElement('legend');
   legend.textContent = legendText;
 
   fieldset.appendChild(legend);
 
   return fieldset;
+}
+
+function createRemoveBtn() {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+</svg>`;
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-icon');
+  button.setAttribute('id', 'remove-question');
+  button.innerHTML = svg;
+  button.addEventListener('click', (e) => {
+    handleRemove(e);
+  });
+
+  return button;
 }
 
 function createLabel(htmlFor, labelText) {
