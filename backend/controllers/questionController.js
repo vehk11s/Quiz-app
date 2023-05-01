bedf8901d430906554a22e5361430c2962f6ca42
mintteal <minttu.laesmaa@student.hamk.fi>
2023-05-01 21:38:18 +0300

    Validation of posting new questions

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const Question = require('../models/questionModel');
+const Category = require('../models/categoryModel');
+const { body, validationResult } = require('express-validator');
+
+/* 
+Validation test category ids:
+Acc = 6447f81e6f1b5ca81f7a5506
+N/A = 6447f81e6f1b5ca81f7a5507
+INV = 6447f81e6f1b5ca81f7abc5506
+*/
 
 // GET questions of given category
 exports.get_questions = async function (req, res) {
@@ -39,16 +48,48 @@ exports.get_question = function (req, res) {
 
 // POST new question
 // TODO: Validation
-exports.add_question = async function (req, res) {
-  category = new mongoose.Types.ObjectId(req.body.category);
+exports.add_question = [
+  body('question', "Question can't be empty").trim().notEmpty().isString(),
+  body('type', 'Question type must be defined')
+    .trim()
+    .default('multichoice')
+    .isString(),
+  body('options', "Options can't be empty")
+    .if(body('type').equals('multichoice'))
+    .isArray({ min: 4, max: 4 })
+    .withMessage('Wrong amount of options'),
+  body('explanation').trim().default(''),
+  body('hint').trim().default(''),
 
-  // Prevents additional documents from being inserted if one fails
-  const insertOptions = { ordered: true };
+  async (req, res) => {
+    try {
+      // Casting category id to ObjectId throws error if argument is in invalid format
+      const categoryId = new mongoose.Types.ObjectId(req.body.category);
+      const category = await Category.findById(categoryId);
 
-  await Question.insertMany(req.body, insertOptions)
-    .then((saved) => res.json(saved))
-    .catch((error) => res.send(error));
-};
+      // Manually handle the empty document MongoDB returns if category is not found
+      if (category === null) {
+        throw new Error('Category not found');
+      }
+
+      try {
+        validationResult(req).throw();
+        res.status(200).send(req.body);
+
+        const insertOptions = { ordered: true }; // Prevents additional documents from being inserted if one fails
+
+        await Question.insertMany(req.body, insertOptions)
+          .then((saved) => res.json(saved))
+          .catch((error) => res.send(error));
+      } catch (e) {
+        console.log(e);
+        res.status(400).send({ errors: e.mapped() });
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+];
 
 // EDIT question by id
 exports.edit_question = function (req, res) {
