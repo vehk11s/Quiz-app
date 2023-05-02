ee2a4cdb816c4d3d3954468cd990b09a65e7ce03
mintteal <minttu.laesmaa@student.hamk.fi>
2023-05-02 17:36:39 +0300

    Questin API validation
    Question API validation and sanitation for all api call data

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,121 +1,200 @@
 const mongoose = require('mongoose');
 const Question = require('../models/questionModel');
 const Category = require('../models/categoryModel');
-const { body, validationResult } = require('express-validator');
+const {
+  check,
+  body,
+  query,
+  param,
+  validationResult,
+} = require('express-validator');
 
-/* 
-Validation test category ids:
-Acc = 6447f81e6f1b5ca81f7a5506
-N/A = 6447f81e6f1b5ca81f7a5507
-INV = 6447f81e6f1b5ca81f7abc5506
-*/
+// GET all questions of given category
+exports.get_questions = [
+  query('category', 'Invalid category id')
+    .exists()
+    .isMongoId()
+    .custom((val) => Category.isValidCategory(val)),
 
-// GET questions of given category
-exports.get_questions = async function (req, res) {
-  let categoryId = new mongoose.Types.ObjectId(req.query.category);
+  async function (req, res) {
+    try {
+      validationResult(req).throw();
 
-  Question.find({ category: categoryId })
-    .populate('category')
-    .then((result) => {
-      if (result.length < 5) {
-        res.status(418).send('Too few questions');
-      } else {
-        res.status(200).json(result);
-      }
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-};
+      let categoryId = new mongoose.Types.ObjectId(req.query.category);
 
-// GET single by id
-exports.get_question = function (req, res) {
-  let questionId = new mongoose.Types.ObjectId(req.params.id);
+      await Question.find({ category: categoryId })
+        .populate('category')
+        .then((result) => {
+          if (result.length < 5) {
+            res.status(400).send('Too few questions');
+          } else {
+            res.status(200).json(result);
+          }
+        })
+        .catch((error) => {
+          res.status(400).send(error);
+        });
+    } catch (error) {
+      res.status(400).send({ errors: error.mapped() });
+    }
+  },
+];
 
-  Question.findById(questionId)
-    .populate('category')
-    .then((result) => {
-      if (result === null) {
-        res.status(418).send('Question not found');
-      } else {
-        res.status(200).json(result);
-      }
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-};
+// GET single question by id
+exports.get_question = [
+  param('id', 'Invalid question id')
+    .exists()
+    .isMongoId()
+    .custom((val) => Question.isValidQuestion(val)),
+
+  async function (req, res) {
+    try {
+      validationResult(req).throw();
+
+      let questionId = new mongoose.Types.ObjectId(req.params.id);
+
+      Question.findById(questionId)
+        .populate('category')
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
+    } catch (error) {
+      res.status(400).send({ errors: error.mapped() });
+    }
+  },
+];
 
 // POST new question
-// TODO: Validation
 exports.add_question = [
-  body('question', "Question can't be empty").trim().notEmpty().isString(),
+  body('category', 'Invalid category id')
+    .exists()
+    .isMongoId()
+    .custom((val) => Category.isValidCategory(val))
+    .bail(),
+  body('question', "Question can't be empty")
+    .trim()
+    .notEmpty()
+    .isString()
+    .escape()
+    .bail(),
   body('type', 'Question type must be defined')
     .trim()
     .default('multichoice')
-    .isString(),
+    .isString()
+    .bail(),
   body('options', "Options can't be empty")
     .if(body('type').equals('multichoice'))
     .isArray({ min: 4, max: 4 })
-    .withMessage('Wrong amount of options'),
-  body('explanation').trim().default(''),
-  body('hint').trim().default(''),
+    .withMessage('Wrong amount of options')
+    .bail(),
+  check('options.*.option', "Option can't be empty")
+    .trim()
+    .notEmpty()
+    .isString()
+    .escape()
+    .bail(),
+  check('options.*.isCorrect', 'Option answer must be true or false')
+    .trim()
+    .notEmpty()
+    .isString()
+    .isIn(['true', 'false'])
+    .bail(),
+  body('explanation').trim().escape().default(''),
+  body('hint').trim().escape().default(''),
 
   async (req, res) => {
     try {
-      // Casting category id to ObjectId throws error if argument is in invalid format
-      const categoryId = new mongoose.Types.ObjectId(req.body.category);
-      const category = await Category.findById(categoryId);
+      validationResult(req).throw();
 
-      // Manually handle the empty document MongoDB returns if category is not found
-      if (category === null) {
-        throw new Error('Category not found');
-      }
-
-      try {
-        validationResult(req).throw();
-        res.status(200).send(req.body);
-
-        const insertOptions = { ordered: true }; // Prevents additional documents from being inserted if one fails
-
-        await Question.insertMany(req.body, insertOptions)
-          .then((saved) => res.json(saved))
-          .catch((error) => res.send(error));
-      } catch (e) {
-        console.log(e);
-        res.status(400).send({ errors: e.mapped() });
-      }
+      await Question.insertMany(req.body, { ordered: true })
+        .then((saved) => res.json(saved))
+        .catch((error) => res.send(error));
     } catch (error) {
-      res.status(400).send(error.message);
+      res.status(400).send({ errors: error.mapped() });
     }
   },
 ];
 
 // EDIT question by id
-exports.edit_question = function (req, res) {
-  let questionId = new mongoose.Types.ObjectId(req.params.id);
-  let { question, type, options, explanation, hint, category } = req.body;
+exports.edit_question = [
+  body('category', 'Invalid category id')
+    .exists()
+    .isMongoId()
+    .custom((val) => Category.isValidCategory(val))
+    .optional(),
+  body('question', "Question can't be empty")
+    .trim()
+    .notEmpty()
+    .isString()
+    .escape()
+    .optional(),
+  body('type', 'Question type must be defined')
+    .trim()
+    .default('multichoice')
+    .isString()
+    .optional(),
+  body('options', "Options can't be empty")
+    .if(body('type').equals('multichoice'))
+    .isArray({ min: 4, max: 4 })
+    .withMessage('Wrong amount of options')
+    .optional(),
+  check('options.*._id', 'Invalid option id').exists().isMongoId().optional(),
+  check('options.*._option', "Option can't be empty").escape().optional(),
+  check('options.*.isCorrect', 'Option answer must be true or false')
+    .trim()
+    .notEmpty()
+    .isString()
+    .isIn(['true', 'false'])
+    .optional(),
+  body('explanation').trim().default('').optional(),
+  body('hint').trim().default('').optional(),
 
-  Question.findByIdAndUpdate(
-    questionId,
-    {
-      $set: { question, type, options, explanation, hint, category },
-    },
-    { new: true }
-  ).then((updatedQuestion) => {
-    res.json(updatedQuestion);
-  });
-};
+  async function (req, res) {
+    try {
+      validationResult(req).throw();
+
+      let questionId = new mongoose.Types.ObjectId(req.params.id);
+      let { question, type, options, explanation, hint, category } = req.body;
+
+      Question.findByIdAndUpdate(
+        questionId,
+        {
+          $set: { question, type, options, explanation, hint, category },
+        },
+        { new: true }
+      )
+        .then((updatedQuestion) => {
+          res.json(updatedQuestion);
+        })
+        .catch((error) => res.send(error));
+    } catch (error) {
+      res.status(400).send({ errors: error.mapped() });
+    }
+  },
+];
 
 // DELETE
-exports.delete_question = function (req, res) {
-  let questionId = new mongoose.Types.ObjectId(req.params.id);
+exports.delete_question = [
+  param('id', 'Invalid question id')
+    .exists()
+    .isMongoId()
+    .custom((val) => Question.isValidQuestion(val)),
 
-  Question.findByIdAndDelete(questionId).then((result) => {
-    if (result === null) {
-      res.status(418).send('Question not found');
-    } else {
-      res.status(200).send(result);
+  async function (req, res) {
+    try {
+      validationResult(req).throw();
+      let questionId = new mongoose.Types.ObjectId(req.params.id);
+
+      await Question.findByIdAndDelete(questionId)
+        .then((result) => {
+          res.status(200).send(`Deleted question with id ${req.params.id}`);
+        })
+        .catch((error) => res.status(400).send(error));
+    } catch (error) {
+      res.status(400).send({ errors: error.mapped() });
     }
-  });
-};
+  },
+];
--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
   category: { type: String, required: true },
 });
+
+categorySchema.statics = {
+  isValidCategory(id) {
+    return this.findById(id).then((result) => {
+      if (!result) throw new Error('Category not found');
+    });
+  },
+};
 
 /* 
 transform returned object's id to a more readable format by
