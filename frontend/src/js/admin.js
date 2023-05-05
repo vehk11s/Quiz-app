08b74847a6ac3a375819f3c4b4941bc06f1469b0
mintteal <minttu.laesmaa@student.hamk.fi>
2023-05-05 19:05:45 +0300

    New layout

--- frontend/src/js/admin.js
+++ frontend/src/js/admin.js
@@ -3,182 +3,206 @@ import { getCategories } from '../api/categoryApi.js';
 import { drawForm } from './questionForm.js';
 import { categoryForm, categoryDeleting } from './categoryForm.js';
 
-let categoryBtn = document.querySelector('#category-btn');
-let questionBtn = document.querySelector('#question-btn');
-
-// Prevents throwing user back to main page on load
-// Doesn't yet retain category-based question listing
-window.addEventListener('load', () => {
+/* 
+Runs when the page is refreshed. 
+The function fetches categories and checks the window hash location and finds the selected category.
+After that it draws the sidebar and site content
+*/
+window.addEventListener('load', async () => {
+  // Fetch categories on load
+  const categories = await getCategories();
   const location = window.location.hash.substring(1);
-  init(location);
+
+  // Find the category that was selected before load
+  const selectedCategory = categories.find(
+    (category) => category.category.toLowerCase() === location
+  );
+
+  drawSidebar(categories);
+  drawContent(selectedCategory);
 });
 
-categoryBtn.addEventListener('click', () => {
-  window.location.hash = '#categories';
-  init('categories');
-});
+/* 
+Runs when the page is refreshed.
+Draws sidebar, category nav and control buttons for adding new categories and questions */
+async function drawSidebar(categories) {
+  const sidebar = document.querySelector('aside');
+  const categoryList = sidebar.querySelector('.nav-categories');
 
-questionBtn.addEventListener('click', () => {
-  window.location.hash = '#questions';
-  init('questions');
-});
+  categories.forEach((category) => {
+    const listItem = document.createElement('li');
+    const btn = document.createElement('button');
 
-function init(location) {
-  const section = document.querySelector('section');
-  section.innerHTML = '';
+    btn.id = category.id;
+    btn.classList.add('btn', 'btn-list', 'btn-sm');
+    btn.textContent = category.category;
 
-  const title = document.createElement('h1');
-  const text = document.createElement('p');
-
-  switch (location) {
-    case 'questions':
-      title.textContent = 'Questions';
-      text.textContent = 'Select a category to view questions';
-
-      break;
-    case 'categories':
-      title.textContent = 'Categories';
-      text.textContent = 'All categories listed. You can add new from the button below.';
-      break;
-    default:
-      title.textContent = 'Admin';
-      text.textContent = 'Admin site';
-      break;
-  }
-
-  section.appendChild(title);
-  section.appendChild(text);
-
-  if (location === 'questions') {
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-primary');
-    button.textContent = 'Add new questions';
-    button.addEventListener('click', () => {
-      drawForm();
-    });
-    section.appendChild(button);
-  }
-
-  // Category
-  if (location === 'categories') {
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-primary');
-    button.textContent = 'Add new category';
-    button.addEventListener('click', () => {
-      categoryForm();
-    });
-    section.appendChild(button);
-
-    listCategories();
-  }
-
-  handleCategoryNav(location);
-}
-
-async function handleCategoryNav(location) {
-  const body = document.querySelector('body');
-  const categoryNav = body.querySelector('#category-buttons');
-
-  if (location != 'questions') {
-    body.removeChild(categoryNav);
-  }
-
-  if (!categoryNav) {
-    const main = document.querySelector('#main-nav');
-
-    const categoryRow = document.createElement('div');
-    categoryRow.classList.add('row');
-    categoryRow.id = 'category-buttons';
-
-    const categories = await getCategories();
-    categories.forEach((category) => {
-      const btn = document.createElement('button');
-      btn.classList.add('btn', 'btn-secondary', 'btn-sm');
-
-      btn.id = category.id;
-      btn.textContent = category.category;
-
-      categoryRow.appendChild(btn);
-
-      main.after(categoryRow);
+    btn.addEventListener('click', async () => {
+      window.location.hash = btn.textContent.toLowerCase();
+      const questions = await getQuestions({ category: btn.id });
+      listQuestions(questions, category);
     });
 
-    setListeners();
-  }
-}
+    listItem.appendChild(btn);
+    categoryList.appendChild(listItem);
+  });
 
-function setListeners() {
-  const parent = document.querySelector('#category-buttons');
-  const buttons = parent.querySelectorAll('button');
+  // Set up listeners for adding new elements
+  const categoryBtn = document.querySelector('#category-btn');
+  categoryBtn.addEventListener('click', () => {
+    categoryForm();
+  });
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', async () => {
-      const questions = await getQuestions({ category: button.id });
-      createContent(questions);
-    });
+  const questionBtn = document.querySelector('#question-btn');
+  questionBtn.addEventListener('click', () => {
+    drawForm();
   });
 }
 
-function createContent(elements) {
+/* 
+Runs when the page is refreshed.
+If user had selected a category when refreshing the page, this function
+fetches the correct data and renders the list.
+*/
+async function drawContent(selectedCategory) {
+  // Find parent element and clear content
   const section = document.querySelector('section');
-  section.innerHTML = ''; // Remove content
 
-  const list = document.createElement('ol'); // Main listing
+  // Set page title
+  const sectionHeader = section.querySelector('.section__header');
+  const sectionTitle = sectionHeader.querySelector('h1');
 
-  elements.forEach((element) => {
-    const questionItem = document.createElement('li');
-    questionItem.textContent = element.question;
-    questionItem.classList.add('list__question');
+  if (selectedCategory) {
+    sectionTitle.textContent = selectedCategory.category;
+    const questions = await getQuestions({ category: selectedCategory.id });
 
-    const optionsList = document.createElement('ol');
-
-    element.options.forEach((option) => {
-      const optionItem = document.createElement('li');
-
-      if (option.isCorrect) {
-        optionItem.textContent = `${option.option}`;
-      } else {
-        optionItem.textContent = `${option.option}`;
-      }
-
-      optionsList.appendChild(optionItem);
-    });
-
-    questionItem.appendChild(optionsList);
-
-    const listFooter = document.createElement('div');
-    listFooter.classList.add('row', 'list__footer');
-
-    const delBtn = createBtn('delete', element.id);
-    delBtn.addEventListener('click', (e) => {
-      deleteQuestion(e.target.value);
-    });
-
-    const editBtn = createBtn('edit', element.id);
-    editBtn.addEventListener('click', (e) => {
-      drawForm(e.target.value);
-    });
-
-    listFooter.appendChild(editBtn);
-    listFooter.appendChild(delBtn);
-
-    questionItem.appendChild(listFooter);
-    list.appendChild(questionItem);
-    section.appendChild(list);
-  });
+    listQuestions(questions, selectedCategory);
+  } else {
+    sectionTitle.textContent = 'Admin';
+  }
 }
 
-function createBtn(type, id) {
+/* 
+Runs when the page is refreshed and when a category button is clicked.
+Updates the section title, calls drawHeaderButtons and handles the rendering of
+question listing
+*/
+function listQuestions(questions, selectedCategory) {
+  // Update site header
+  const sectionTitle = document.querySelector('h1');
+  sectionTitle.textContent = selectedCategory.category;
+
+  drawHeaderButtons(selectedCategory);
+
+  // Find section content, remove previous
+  const sectionContent = document.querySelector('.section__content');
+  sectionContent.innerHTML = ''; // Remove content
+
+  // Draw list
+  const list = document.createElement('ol');
+
+  if (questions.length === 0) {
+    const text = document.createElement('p');
+    text.textContent = "This category doesn't have any questions yet";
+    sectionContent.appendChild(text);
+  } else {
+    // Loop all questions
+    questions.forEach((element) => {
+      // Draw list item
+      const questionItem = document.createElement('li');
+      questionItem.textContent = element.question;
+      questionItem.classList.add('list__question');
+
+      // Draw nested list
+      const optionsList = document.createElement('ol');
+
+      // Draw list items for options
+      element.options.forEach((option) => {
+        const optionItem = document.createElement('li');
+        optionItem.textContent = `${option.option}`;
+        optionsList.appendChild(optionItem);
+      });
+
+      questionItem.appendChild(optionsList);
+
+      const listFooter = document.createElement('div');
+      listFooter.classList.add('row', 'list__footer');
+
+      const delBtn = createBtn('delete', '', element.id);
+      delBtn.addEventListener('click', (e) => {
+        deleteQuestion(e.target.value);
+      });
+
+      const editBtn = createBtn('edit', '', element.id);
+      editBtn.addEventListener('click', (e) => {
+        drawForm(e.target.value);
+      });
+
+      listFooter.appendChild(editBtn);
+      listFooter.appendChild(delBtn);
+
+      questionItem.appendChild(listFooter);
+      list.appendChild(questionItem);
+      sectionContent.appendChild(list);
+    });
+  }
+}
+
+/* 
+Runs when the page is refreshed and when a category button is clicked.
+Renders control buttons below the section title
+*/
+function drawHeaderButtons(category) {
+  const sectionHeader = document.querySelector('.section__header');
+
+  // Remove element if it already exists
+  if (document.querySelector('.buttons')) {
+    sectionHeader.removeChild(document.querySelector('.buttons'));
+  }
+  const buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('buttons');
+
+  // Add question, doenst yet recognize the selected category
+  const addQuestionBtn = createBtn('add', 'question', '');
+  addQuestionBtn.addEventListener('click', () => {
+    drawForm();
+  });
+
+  // Edit selected category
+  const editCategoryBtn = createBtn('edit', 'category', category.id);
+  editCategoryBtn.addEventListener('click', (e) => {
+    categoryForm(e.target.value);
+  });
+
+  // Delete selected category
+  const delCategoryBtn = createBtn('delete', 'category', category.id);
+  delCategoryBtn.addEventListener('click', (e) => {
+    categoryDeleting(e.target.value);
+  });
+
+  buttonDiv.appendChild(addQuestionBtn);
+  buttonDiv.appendChild(editCategoryBtn);
+  buttonDiv.appendChild(delCategoryBtn);
+
+  sectionHeader.appendChild(buttonDiv);
+}
+
+// Helper function
+function createBtn(type, elem, id) {
   const button = document.createElement('button');
   button.classList.add('btn', 'btn-secondary', 'btn-sm');
 
   switch (type) {
+    case 'add':
+      button.textContent = `Add ${elem}`;
+      button.value = '';
+      break;
     case 'delete':
-      button.textContent = 'Delete';
+      button.textContent = `Delete ${elem}`;
       button.value = id;
       break;
     case 'edit':
-      button.textContent = 'Edit';
+      button.textContent = `Edit ${elem}`;
       button.value = id;
       break;
     default:
@@ -187,39 +211,4 @@ function createBtn(type, id) {
   }
 
   return button;
-}
-
-async function listCategories() {
-  const section = document.querySelector('section');
-  const list = document.createElement('ol'); // Main listing
-
-  const categories = await getCategories();
-
-  categories.forEach(function(object){
-    const categoryItem = document.createElement('li');
-
-    categoryItem.textContent = object.category;
-    categoryItem.classList.add('list__categories');
-
-    const listFooter = document.createElement('div');
-    listFooter.classList.add('row', 'list__footer');
-
-    const delBtn = createBtn('delete', object.id);
-    delBtn.addEventListener('click', (e) => {
-      categoryDeleting(e.target.value);
-    });
-
-    const editBtn = createBtn('edit', object.id);
-    editBtn.addEventListener('click', (e) => {
-      categoryForm(e.target.value);
-    });
-
-    listFooter.appendChild(editBtn);
-    listFooter.appendChild(delBtn);
-
-    categoryItem.appendChild(listFooter);
-    list.appendChild(categoryItem);
-    section.appendChild(list);
-
-  });
 }
